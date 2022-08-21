54d5be541c818e1681b070180489d96b22ece994
Claeb101 <calebarulandu@gmail.com>
2022-08-21 16:19:16 -0400

    upsolve kickstart E: D subtask 2

--- archive/Google/kickstart/2022/E/D.cpp
+++ archive/Google/kickstart/2022/E/D.cpp
@@ -3,15 +3,25 @@
 using namespace std;
 #define ll long long
 
-// SUBTASK 1: Simulate dp
+const ll MAXN = 10, MAXM = 20, MAXP = 10;
+const ll inf = 1e18;
+ll dp[MAXN][MAXN][MAXM+1][(1LL<<MAXP)]; // dp[r][c][m][p] = max coins at (r, c) after m minutes with p delivery bitmask
+ll N, P, M;
+pair<ll, ll> A;
+map<pair<int, int>, pair<int, int>> cs; // cs[{r, c}] = {#, coins}
+
+// STAY, NORTH, EAST, WEST, SOUTH 
+const vector<pair<ll, ll>> deltas = {{0, 0}, {-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+vector<pair<char, ll>> tolls(5, {'+', 0}); 
 
 // could've gotten top 200 if i didn't forget this :((
 ll fdiv(ll x, ll d){
-  return x < 0 ? (x-d+1)/d : x/d;
+  return x < 0 ? (x-d+1)/d : x/d; // integer division for negatives
 }
 
-// toll roads
-ll toll(ll x, pair<char, ll> &f){
+// compute toll from dir
+ll toll(ll x, int dir){
+  pair<char, ll> f = tolls[dir];
   if(f.first == '/'){
     return fdiv(x, f.second);
   } else if(f.first == '*'){
@@ -30,60 +40,80 @@ void ckmax(ll& target, ll val){
   target = max(target, val);
 }
 
+// O(N^2 * M * 2^P)
 void solve(){
-  // read
-  const ll inf = 1e18;
-  ll N, P, M;
-  const ll MAXN = 10, MAXM = 20;
-  pair<ll, ll> A;
-  ll dp[MAXN][MAXN][MAXM+1];
+  // clear
+  cs.clear();
 
+  // read
   cin >> N >> P >> M >> A.first >> A.second;
   A.first--; A.second--;
-
-  // NORTH, EAST, WEST, SOUTH | 0, 1, 2, 3
-  vector<pair<char, ll>> tolls(4);
-  for(ll i = 0; i < 4; i++) {
+  for(ll i = 1; i <= 4; i++) {
     cin >> tolls[i].first >> tolls[i].second;
   }
-
+  for(int p = 0; p < P; p++) {
+    pair<int, int> pos; cin >> pos.first >> pos.second;
+    pos.first--; pos.second--;
+    int x; cin >> x;
+    cs[pos] = {p, x};
+  }
+  
   // init
+  ll PS = 1LL << P;
   for(ll r = 0; r < N; r++){
     for(ll c = 0; c < N; c++){
       for(ll m = 0; m <= M; m++){
-        dp[r][c][m] = -inf;
-      }
-    }
-  }
-  dp[A.first][A.second][0] = 0;
-
-  ll bst = 0;
-
-  // simulate toll nav
-  for(ll m = 0; m < M; m++){
-    for(ll r = 0; r < N; r++){
-      for(ll c = 0; c < N; c++){
-        assert(dp[r][c][m] >= -inf);
-        if(dp[r][c][m] != -inf){
-          if(r > 0)   ckmax(dp[r-1][c+0][m+1], toll(dp[r][c][m], tolls[0]));
-          if(c < N-1) ckmax(dp[r+0][c+1][m+1], toll(dp[r][c][m], tolls[1]));
-          if(c > 0)   ckmax(dp[r+0][c-1][m+1], toll(dp[r][c][m], tolls[2]));
-          if(r < N-1) ckmax(dp[r+1][c+0][m+1], toll(dp[r][c][m], tolls[3]));
-          
-          ckmax(bst, dp[r][c][m]);
-          dp[r][c][m+1] = max(dp[r][c][m+1], dp[r][c][m]);
+        for(ll p = 0; p < PS; p++){
+          dp[r][c][m][p] = -inf;
         }
       }
     }
   }
+  dp[A.first][A.second][0][0] = 0;
 
-  // get best
+  // step through minutes
+  for(ll m = 0; m < M; m++){
+    for(ll r = 0; r < N; r++){
+      for(ll c = 0; c < N; c++){
+        for(ll p = 0; p < PS; p++){
+          if(dp[r][c][m][p] != -inf){
+            // check each direction
+            for(int di = 0; di <= 4; di++){
+              int rp = r + deltas[di].first, cp = c + deltas[di].second;
+              if(rp >= 0 && cp >= 0 && rp < N && cp < N){
+                // move + deliver
+                if(cs.count({rp, cp}) == 1){
+                  auto customer = cs[{rp, cp}];
+                  if(((p >> customer.first) & 1LL) == 0LL){
+                    ll pp = p | (1LL << customer.first);
+                    ckmax(dp[rp][cp][m+1][pp], toll(dp[r][c][m][p], di)+customer.second);
+                  }
+                }
+
+                // move. don't deliver
+                ckmax(dp[rp][cp][m+1][p], toll(dp[r][c][m][p], di));
+              }
+            }
+          }
+        }
+      } 
+    }
+
+  }
+
+  // get best when pizza bitmask is all 1
+  ll bst = -inf;
+  ll FP = P == 0 ? 0 : (PS-1);
   for(ll r = 0; r < N; r++) {
     for(ll c = 0; c < N; c++){
-      ckmax(bst, dp[r][c][M]);
+      ckmax(bst, dp[r][c][M][FP]);
     }
   }
-  cout << bst << '\n';
+
+  // output
+  if(bst == -inf) cout << "IMPOSSIBLE";
+  else cout << bst;
+  cout << '\n';
 }
 
 int main(){
--- src/stress.sh
+++
@@ -1,9 +0,0 @@
-# A and B are executables you want to compare, gen takes int
-# as command line arg. Usage: 'sh stress.sh'
-for ((i = 1; ; ++i)); do  # if they are same then will loop forever
-    echo $i
-    python3 gen.py $i > int;
-    ./Cb < int > out1
-    ./C < int > out2
-    diff -w out1 out2 || break
-done
